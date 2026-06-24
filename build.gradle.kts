c8565664f1295c9207656eb183f59438bfc61326
Woder <17339354+wode490390@users.noreply.github.com>
2026-06-24 23:33:32 +0800

    Bump Java version to 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 allprojects {
     plugins.apply("java-library")
-    val javaLanguageVersion = JavaLanguageVersion.of(21)
+    val javaLanguageVersion = JavaLanguageVersion.of(25)
     the<JavaPluginExtension>().apply {
         toolchain {
             languageVersion = javaLanguageVersion
